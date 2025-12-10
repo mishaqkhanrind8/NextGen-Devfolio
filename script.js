bd7f23bb7b4602bff4c4534acdaa566528862dcc
Muhammad Ishaq <aonemobilesncomputers786@gmail.com>
2025-12-10 22:05:26 +0500

    main

---
+++ script.js
@@ -0,0 +1,9 @@
+  function openNav() {
+    document.getElementById("mobileSidebar").style.display = "block";
+  }
+
+  function closeNav() {
+    document.getElementById("mobileSidebar").style.display = "none";
+  }
+
+  
